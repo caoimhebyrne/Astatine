897a764e718418d261baf312de0d0fa9a97e1cc7
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-03-03 16:54:27 +0000

    Fix MC-72687: Action Bar Messages do not have shadow under the text Thanks to @LlamaLad7 for assisting with the fix

---
+++ src/main/java/dev/dreamhopping/astatine/mixins/InGameHudMixin.java
@@ -0,0 +1,57 @@
+/*
+ *     Astatine is a mod for Minecraft which fixes many common bugs in the game
+ *     Copyright (C) 2021 Conor Byrne
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dev.dreamhopping.astatine.mixins;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+/**
+ * Fix MC-72687: Action Bar Messages do not have the small shadow under the text
+ *
+ * @author Conor Byrne (dreamhopping) & LlamaLad7
+ */
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    /**
+     * Redirects all [TextRenderer.draw] calls to [TextRenderer.drawWithShadow] after the Profiler#push method is called with "overlayMessage" as the argument
+     */
+    @Redirect(method = "render",
+            slice = @Slice(
+                    from = @At(
+                            value = "INVOKE_STRING",
+                            target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V",
+                            args = "ldc=overlayMessage"
+                    )
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I",
+                    ordinal = 0
+            )
+    )
+    private int drawToDrawWithShadow(TextRenderer textRenderer, MatrixStack matrices, Text text, float x, float y, int color) {
+        return textRenderer.drawWithShadow(matrices, text, x, y, color);
+    }
+}
