a74d6b78bb62f10111651e853eec99f8b4c2a8da
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2021-03-14 18:52:30 -0400

    Add comments to MC-117800 fix.

--- src/main/java/dev/dreamhopping/astatine/mixins/BedBlockMixin.java
+++ src/main/java/dev/dreamhopping/astatine/mixins/BedBlockMixin.java
@@ -8,9 +8,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Fix MC-117800: Half bed can be placed outside the world's border
+ *
+ * @author DJtheRedstoner
+ */
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
 
+    /**
+     * Add an additional check when checking if the 2nd part of bed can be placed that adds a check if
+     * its inside current world border.
+     */
     @Redirect(
         method = "getPlacementState",
         at = @At(
@@ -19,7 +28,9 @@ public class BedBlockMixin {
         )
     )
     private boolean canReplace(BlockState blockState, ItemPlacementContext ctx) {
+        // Get the position of the 2nd part of bed
         BlockPos targetPos = ctx.getBlockPos().offset(ctx.getPlayerFacing());
+        // Do default check as well as world border check
         return blockState.canReplace(ctx) && ctx.getWorld().getWorldBorder().contains(targetPos);
     }
 
