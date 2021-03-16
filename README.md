610aa3ec90c35195c7cfd22e750f5c3c50dc1c9c
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2021-03-16 15:13:40 -0400

    Fix MC-153010: "doMobLoot" gamerule doesn't prevent foxes from dropping their items (#8)

--- README.md
+++ README.md
@@ -32,12 +32,12 @@ Astatine aims to fix those bugs in the most efficient ways, without compromising
 - ``MC-72494``: In Statistics screen 'm' is the same unit for both minutes and meters
 - ``MC-142555``: Cats and Ocelots wont eat tropical fish
 - ``MC-117800``: Half bed can be placed outside the world's border
+- ``MC-153010``: "doMobLoot" gamerule doesn't prevent foxes from dropping their items
 
 ### Bugs to be fixed
 
 - ``MC-149058``: Using a 1-9 hotkey to move items into the anvil appends the number to the new item name
 - ``MC-219537``: Long villager names cause an overlay
-- ``MC-153010``: "doMobLoot" gamerule doesn't prevent foxes from dropping their items
 - ``MC-201723``: Statistics sprites don't look pressed when clicked
 
 ### Credits
---
+++ src/main/java/dev/dreamhopping/astatine/mixins/FoxEntityMixin.java
@@ -0,0 +1,65 @@
+/*
+ *     Astatine is a mod for Minecraft which fixes many common bugs in the game
+ *     Copyright (C) 2021 Conor Byrne
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.dreamhopping.astatine.mixins;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Fix MC-153010: "doMobLoot" gamerule doesn't prevent foxes from dropping their items
+ *
+ * @author DJtheRedstoner
+ */
+@Mixin(FoxEntity.class)
+public abstract class FoxEntityMixin extends Entity {
+
+    /**
+     * Required constructor for mixin to function
+     */
+    public FoxEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    /**
+     * Adds an additional check when dropping foxes held items
+     *
+     * @return false to continue dropping item, true to cancel
+     */
+    @Redirect(
+        method = "drop",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"
+        )
+    )
+    private boolean shouldDrop(ItemStack itemStack) {
+        // Do the default check we redirect
+        if (itemStack.isEmpty()) return false;
+        // Add an additional check for the doMobLoot gamerule
+        return !world.getGameRules().getBoolean(GameRules.DO_MOB_LOOT);
+    }
+
+}
